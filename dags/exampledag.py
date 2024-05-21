b75600ba7c17a4162b6b6d9a36d338ea1b26d3aa
kentdanas <kentdanas@gmail.com>
2024-05-21 10:22:30 -0700

    Update retries

--- dags/exampledag.py
+++ dags/exampledag.py
@@ -32,7 +32,7 @@ import requests
     schedule="@daily",
     catchup=False,
     doc_md=__doc__,
-    default_args={"owner": "Astro", "retries": 3},
+    default_args={"owner": "Astro", "retries": 2},
     tags=["example"],
 )
 def example_astronauts():
