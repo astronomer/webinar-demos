f02b8cb48d5a9fd5d8f2d112ce801b15b41d65d4
TJaniF <tamara.fingerlin@astronomer.io>
2025-06-24 12:25:09 +0200

    update

--- dags/syntax/example_syntax_task_agent.py
+++ dags/syntax/example_syntax_task_agent.py
@@ -26,7 +26,7 @@ weather_report_agent = Agent(
     params={
         "location": Param(
             type="string",
-            default="Basel",
+            default="Bern",
         ),
     },
 )
